4d2e060ff222fb3de38a5639ed9b55521ae210ac
LucaasAndrade <lucasmarcelodeandradesilva@gmail.com>
2022-05-28 18:35:45 -0300

    Temperatura ok

--- src/pages/temperatura/index.js
+++ src/pages/temperatura/index.js
@@ -1,13 +1,24 @@
-
-
+import axios from "axios";
+import { useState } from "react"
 
 
 
 export default function Index(){
+    const [temperatura, setTemperatura] = useState(0);
+    const [resposta, setResposta] = useState('');
+
+    async function verificarTemperatura(){
+        const resp = await axios.get(`http://localhost:5000/temperatura?t=${temperatura}`);
+        setResposta(resp.data.temperatura)
+    }
     return (
         <main>
             <h1> Temperatura </h1>
-            <input></input>
+            Temperatura: <input type='number' value={temperatura} onChange={e => setTemperatura(e.target.value)}/>
+
+            <button onClick={verificarTemperatura}> Verificar Temperatura </button>
+
+            <p>{resposta}</p>
         </main>
     )
 }
